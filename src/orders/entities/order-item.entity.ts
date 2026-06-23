3f70c8c0cb9ac8d905e5bd78d0aace982ccd95e3
Milos <mbojanic53@gmail.com>
2026-06-23 23:19:46 +0200

    feat: add order and order-item entities

---
+++ src/orders/entities/order-item.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Order } from './order.entity';
+
+@Entity('order_items')
+export class OrderItem {
+  @PrimaryGeneratedColumn('uuid')
+    id!: string;
+
+  @Column()
+    productId!: string;
+
+  @Column()
+    name!: string;
+
+  @Column('decimal', { precision: 18, scale: 6 })
+    price!: number;
+
+  @Column('int')
+    quantity!: number;
+
+  @ManyToOne(() => Order, (order) => order.items, { onDelete: 'CASCADE' })
+    order!: Order;
+}

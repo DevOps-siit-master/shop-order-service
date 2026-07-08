99c04d3d491b1eb643666d7642559fd646fbae7f
Milos <mbojanic53@gmail.com>
2026-07-08 22:58:08 +0200

    feat: add health check with database ping

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrdersModule } from './orders/orders.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { OrdersModule } from './orders/orders.module';
       synchronize: true,
     }),
     OrdersModule,
+    HealthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
